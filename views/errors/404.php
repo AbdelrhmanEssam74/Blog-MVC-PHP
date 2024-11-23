edc5baa0dc9c9267bd04c00b6fe42ef9240e57e6
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-11-23 14:13:05 +0200

    ```plaintext Add initial implementation of user authentication module ```

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,11 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<?php
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
-?>
 <head>
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
